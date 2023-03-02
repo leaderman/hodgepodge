838c705da79ac742306c57710128ea4ee3f88f17
yurun <>
2023-03-02 22:35:00 +0800

    新增公众号启用服务器配置验证接口

---
+++ nodejs/javascript/offiaccount/offiaccount_web.js
@@ -0,0 +1,67 @@
+import http from "http";
+import url from "url";
+import crypto from "crypto";
+
+import Config from "../config.js";
+
+function deepWithWideGet(request, response) {
+  const query = url.parse(request.url, true).query;
+
+  const signature = query.signature;
+  const echostr = query.echostr;
+  const timestamp = query.timestamp;
+  const nonce = query.nonce;
+
+  const token = Config.offiaccount_token_deepwithwide;
+
+  let seeds = [token, timestamp, nonce];
+  seeds.sort();
+  seeds = seeds.join("");
+
+  let shasum = crypto.createHash("sha1");
+  shasum.update(seeds);
+  const sign = shasum.digest("hex");
+
+  if (signature === sign) {
+    text(response, echostr);
+  } else {
+    text(response, "");
+  }
+}
+
+function deepWithWidePost(request, response) {}
+
+function deepWithWide(request, response) {
+  const method = request.method;
+  switch (method) {
+    case "GET":
+      deepWithWideGet(request, response);
+      break;
+
+    case "POST":
+      deepWithWidePost(request, response);
+      break;
+  }
+}
+
+function text(response, text) {
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/plain");
+  response.end(text);
+}
+
+const hostname = Config.offiaccount_web_host;
+const port = Config.offiaccount_web_port;
+
+const server = http.createServer((request, response) => {
+  const path = url.parse(request.url).pathname;
+  switch (path) {
+    case "/offiaccount/deepwithwide":
+      deepWithWide(request, response);
+      break;
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
