82136bc91b8a4c20ead464f24e9e74421c70e4f7
yurun <>
2023-03-17 13:44:39 +0800

    删除 main.go 中的测试代码

--- go/main.go
+++ go/main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"hodgepodge-go/config"
-)
-
 func main() {
-	fmt.Println(config.Properties.MySQL.Host)
-	fmt.Println(config.Properties.MySQL.Port)
-	fmt.Println(config.Properties.MySQL.User)
-	fmt.Println(config.Properties.MySQL.Password)
 }
