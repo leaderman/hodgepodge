cee6552baa9772ce3861103a63fe40bbe3667453
leaderman <miao.work2010@gmail.com>
2024-09-08 12:16:33 +0800

    新增 Python 变量示例

---
+++ python/variable.py
@@ -0,0 +1,21 @@
+# 姓名
+name = "杰克"
+# 年龄
+age = 25
+# 性别
+gender = "男"
+
+print("姓名:", name)
+print("年龄:", age)
+print("性别:", gender)
+
+# 姓名
+name2 = "玛丽"
+# 年龄
+age2 = 23
+# 性别
+gender2 = "女"
+
+print("姓名:", name2)
+print("年龄:", age2)
+print("性别:", gender2)
