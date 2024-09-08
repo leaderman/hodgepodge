914ad0ea270322a4fac368303c43693b91875a5f
leaderman <miao.work2010@gmail.com>
2024-09-08 14:32:32 +0800

    新增 Python 常量示例

---
+++ python/const.py
@@ -0,0 +1,42 @@
+# 1 周有 7 天
+days_per_week = 7
+# 1 天有 24 小时
+hours_per_day = 24
+# 1 小时有 60 分钟
+minutes_per_hour = 60
+# 1 分钟有 60 秒
+seconds_per_minute = 60
+
+print("1 周有", days_per_week, "天")
+print("1 天有", hours_per_day, "小时")
+print("1 小时有", minutes_per_hour, "分钟")
+print("1 分钟有", seconds_per_minute, "秒")
+
+days_per_week = 6
+hours_per_day = 20
+minutes_per_hour = 50
+seconds_per_minute = 50
+
+print("1 周有", days_per_week, "天")
+print("1 天有", hours_per_day, "小时")
+print("1 小时有", minutes_per_hour, "分钟")
+print("1 分钟有", seconds_per_minute, "秒")
+
+# 1 周有 7 天
+DAYS_PER_WEEK = 7
+# 1 天有 24 小时
+HOURS_PER_DAY = 24
+# 1 小时有 60 分钟
+MINUTES_PER_HOUR = 60
+# 1 分钟有 60 秒
+SECONDS_PER_MINUTE = 60
+
+print("1 周有", DAYS_PER_WEEK, "天")
+print("1 天有", HOURS_PER_DAY, "小时")
+print("1 小时有", MINUTES_PER_HOUR, "分钟")
+print("1 分钟有", SECONDS_PER_MINUTE, "秒")
+
+DAYS_PER_WEEK = 6
+HOURS_PER_DAY = 20
+MINUTES_PER_HOUR = 50
+SECONDS_PER_MINUTE = 50
